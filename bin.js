d2418fa57f5a0d97a27184457a44e46014b701a9
suhaangar16 <71168978+suhaangar16@users.noreply.github.com>
2020-10-31 16:48:07 +0530

    Add files via upload

---
+++ bin.js
@@ -0,0 +1,15 @@
+class Bin{
+    constructor(x,y,width,height){
+
+        this.body = Bodies.rectangle(x,y,width,height,{isStatic:true});
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("white");
+        rect(pos.x,pos.y,this.width,this.height);
+    }
+}
